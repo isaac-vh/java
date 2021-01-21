048d27b167bda35a8486df0f64063f856215fc43
isaac van hoogten <isaacvanhoogten@gmail.com>
2021-01-21 11:05:49 +0100

    scriptz

---
+++ 5.js-complete-course/04-dates/01-title-by-hour-one/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /04-dates/01-title-by-hour-one/script.js - 4.1: texte en fonction de l'heure (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var hour = new Date().getHours();
+
+    if(hour >= 18 || hour <= 7) {
+        document.getElementById("target").innerHTML = "Good evening";
+    } else {
+        document.getElementById("target").innerHTML = "Hello";
+    };
+
+})();
---
+++ 5.js-complete-course/04-dates/02-title-by-hour-two/script.js
@@ -0,0 +1,12 @@
+/* becode/javascript
+ *
+ * /04-dates/02-title-by-hour-two/script.js - 4.2: texte en fonction de l'heure (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+document.getElementById("target").innerHTML = new Date().getHours() >= 17 && new Date().getMinutes() >= 30 ? 'Good evening' : 'Hello';
